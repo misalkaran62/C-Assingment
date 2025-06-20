1fa54826e118914d35ea3aa8c0184503a4dfd088
unknown <misalkaran62@gmail.com>
2025-06-20 14:36:50 +0530

    first Programe

---
+++ whetherPositive.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+
+    printf("Enter the Number :");
+    scanf("%d", &num);
+
+    if (num > 0)
+    {
+        printf(" The Number is positve ");
+    }
+    else
+    {
+        printf("The Number is positive");
+    }
+    return 0;
+}
