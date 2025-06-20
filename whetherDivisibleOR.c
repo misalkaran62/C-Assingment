1e0a61e021bcff828e07bf3faae89220dcdf05d4
unknown <misalkaran62@gmail.com>
2025-06-20 17:53:44 +0530

    OR

---
+++ whetherDivisibleOR.c
@@ -0,0 +1,19 @@
+// OR Operater
+
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter the number");
+    scanf("%d", &num);
+
+    if (num % 5 == 0 || num % 7 == 0)
+    {
+        printf("The number is divisible by 5 or 7");
+    }
+    else
+    {
+        printf("The number is Not divisible by 5 or 7");
+    }
+    return 0;
+}
