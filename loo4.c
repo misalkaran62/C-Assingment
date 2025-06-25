6015fb4bdae37ac2ee8deabb65d3068bc6b989f5
unknown <misalkaran62@gmail.com>
2025-06-26 00:12:04 +0530

    100 to 150 hello

---
+++ loo4.c
@@ -0,0 +1,14 @@
+// Write a program to print 100 to 150
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    printf("Enter the Number to print:");
+    scanf("%d", &n);
+
+    for (i = 1; i <= 150; i++)
+    {
+        printf("\nHELLO WORLD!");
+    }
+    return 0;
+}
