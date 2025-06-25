e1443a863e3bd85dc065fb37775d3739981a56c2
unknown <misalkaran62@gmail.com>
2025-06-26 00:06:20 +0530

    1 to 10 hello

---
+++ loop3.c
@@ -0,0 +1,14 @@
+// Write a program to print 1 to 10 
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("Enter the Number to print:");
+    scanf("%d",&n);
+
+    for (i = 1; i <=10; i++)
+    {
+        printf("\nHello World !");
+    }
+    return 0;
+}
